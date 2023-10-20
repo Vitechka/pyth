bd476b45f6877251fbd71c04e1a859196cf689dc
Vitechka <maruga99@mail.ru>
2023-10-20 21:06:05 +0300

    -\_/-

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-
 def print_hi(a):
     print("Vik" * a)
 
+
 print_hi(4)
 
+import random
+
+
+def storm():
+    a = [random.randint(10, 100)]
+    for i in a:
+        b = i // 2
+    return b
+
+
+print(storm())
